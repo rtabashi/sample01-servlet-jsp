69f96bbd0a2f96f5d7a15c4d3e66d17de7650c21
rtabashi <ryoheitabashi@gmail.com>
2021-02-17 22:18:15 +0900

    add mutter logic

--- WebContent/WEB-INF/jsp/main.jsp
+++ WebContent/WEB-INF/jsp/main.jsp
@@ -1,7 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-<%@ page import="model.User" %>
+<%@ page import="model.User,model.Mutter,java.util.List" %>
 <%
 User loginUser = (User)session.getAttribute("loginUser");
+List<Mutter> mutterList = (List<Mutter>)application.getAttribute("mutterList");
+String errorMsg = (String)request.getAttribute("errorMsg");
 %>
 <!DOCTYPE html>
 <html>
@@ -11,8 +13,18 @@ User loginUser = (User)session.getAttribute("loginUser");
 </head>
 <body>
 <h1>メイン</h1>
-<p>
-<%= loginUser.getName() %>さん，ログイン中
-</p>
+<p><%= loginUser.getName() %>さん，ログイン中</p>
+<a href="/sample01/Logout">ログアウト</a>
+<p><a href="/sample01/Main">更新</a></p>
+<form action="/sample01/Main" method="POST">
+<input type="text" name="text">
+<input type="submit" value="つぶやく">
+</form>
+<% if (errorMsg != null) { %>
+	<p><%= errorMsg %></p>
+<% } %>
+<% for (Mutter mutter : mutterList) { %>
+	<p><%= mutter.getUserName() %>：<%= mutter.getText() %></p>
+<% } %>
 </body>
 </html>
--- src/servlet/Main.java
+++ src/servlet/Main.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 @WebServlet("/Main")
@@ -19,12 +20,11 @@ public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		ServletContext application = this.getServletContext();
 		List<Mutter> mutterList = (List<Mutter>)application.getAttribute("mutterList");
 		
 		if (mutterList == null) {
-			mutterList = new ArrayList();
+			mutterList = new ArrayList<>();
 			application.setAttribute("mutterList", mutterList);
 		}
 		
@@ -37,5 +37,29 @@ public class Main extends HttpServlet {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
 			dispatcher.forward(request, response);
 		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+		
+		if (text != null && text.length() != 0) {
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>)application.getAttribute("mutterList");
+			
+			HttpSession session = request.getSession();
+			User loginUser = (User)session.getAttribute("loginUser");
+			
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+			
+			application.setAttribute("mutterList", mutterList);
+		} else {
+			request.setAttribute("errorMsg", "‚Â‚Ô‚â‚«‚ª“ü—Í‚³‚ê‚Ä‚¢‚Ü‚¹‚ñ");
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
 	}
 }
